5f8e3cb44251e186bf88cc52d0b90f6a59c82da0
CoffeeAndCommit <medhavisharma11apr@gmail.com>
2025-11-21 10:12:08 +0530

    worked on data base models

--- learnDjango/learnDjango/urls.py
+++ learnDjango/learnDjango/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from learnDjango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +28,5 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('newapp/', include('newapp.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
-
-    
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#
