997dcd19e7da1cac12bd24ba42d9ec4bfe259aea
CoffeeAndCommit <medhavisharma11apr@gmail.com>
2025-11-21 11:12:52 +0530

    worked on data base models

--- learnDjango/learnDjango/urls.py
+++ learnDjango/learnDjango/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     path('newapp/', include('newapp.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #
--- learnDjango/newapp/views.py
+++ learnDjango/newapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
+from .models import ChaiVariety
+
 # Create your views here.
 
 def all_app(request):
-    return render(request, 'newapp/allapp.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'newapp/allapp.html', {'chais': chais})
